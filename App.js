be9cb6d7ac912b4e694808bdf9e22e905e96f5c2
Adnan Ahmad Khan <47176578+Adnan-Khn@users.noreply.github.com>
2021-12-23 17:56:20 +0530

    Day 2 Assignment

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Container from "./Container/Container.js"
+
+function App(){ //or const Hello = () =>{
+    return(
+        <div>
+            <h1 style={{textAlign: "center"}}>Hello</h1>
+            <Container/>
+        </div>
+        
+    )
+}
+
+export default App
---
+++ Container.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Button from "../Button/Button.js"
+
+function Container(){
+    return(
+        <div>
+            <Button buttonText="Apple"/>
+            <Button buttonText="Mango"/>
+            <Button buttonText="Guava"/>
+        </div>
+    )
+}
+export default Container
